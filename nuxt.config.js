2f51f935cc3c4442c2dd6596f538c9cd3d6086ef
LorexIQ <murashko100500@gmail.com>
2022-10-17 15:50:47 +0300

    fix links favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/icons/Apple.svg' }
+      { rel: 'icon', type: 'image/x-icon', href: `${this.$router.options.base}icons/Apple.svg` }
     ],
     script: [
       {
