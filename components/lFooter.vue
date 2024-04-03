01f3cc0c9647d6b3dfe239d5daf3e5f30f5b5627
LorexIQ <murashko100500@gmail.com>
2024-04-03 17:58:44 +0300

    feat: add ad info

--- components/lFooter.vue
+++ components/lFooter.vue
@@ -2,8 +2,14 @@
   <div class="footer">
     <div class="wrap">
       <div class="footer__info">
-        <img src="~/static/icons/Apple.svg" width="20" height="20" alt="apple">
-        <p>© 2014-{{ new Date().getFullYear() }} AppleService</p>
+        <div class="footer__info__left">
+          <img src="~/static/icons/Apple.svg" width="20" height="20" alt="apple">
+          <p>© 2014-{{ new Date().getFullYear() }} AppleService</p>
+        </div>
+        <div class="footer__info__right">
+          <p>ИП: Коваленко Сергей Юрьевич</p>
+          <p>ИНН: 325300343680</p>
+        </div>
       </div>
     </div>
   </div>
@@ -24,14 +30,25 @@ export default {
   background-color: #000;
   &__info {
     display: flex;
-    align-items: center;
-    gap: 8px;
-    & p {
-      font-size: 16px;
-      margin: 0;
+    justify-content: space-between;
+
+    &__left {
+      display: flex;
+      align-items: center;
+      gap: 8px;
+      & p {
+        font-size: 16px;
+        margin: 0;
+      }
+      & * {
+        color: #adacab;
+      }
     }
-    & * {
-      color: #adacab;
+    &__right {
+      & p {
+        font-size: 16px;
+        margin: 0;
+      }
     }
   }
 }
